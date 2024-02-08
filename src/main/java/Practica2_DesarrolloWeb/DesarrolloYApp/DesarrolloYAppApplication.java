a736c6734ed0d9b1eabec053976f9ade4610b73d
almacasea <almacasea@gmail.com>
2024-02-08 17:55:21 -0600

    Inicio

---
+++ src/main/java/Practica2_DesarrolloWeb/DesarrolloYApp/DesarrolloYAppApplication.java
@@ -0,0 +1,13 @@
+package Practica2_DesarrolloWeb.DesarrolloYApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloYAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloYAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Practica2_DesarrolloWeb/DesarrolloYApp/DesarrolloYAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica2_DesarrolloWeb.DesarrolloYApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloYAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
